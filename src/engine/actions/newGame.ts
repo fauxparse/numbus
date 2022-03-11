37e99febf5bb69de9253cb0390995ded014dedc6
Matt Powell <fauxparse@gmail.com>
2022-03-11 15:32:45 +1300

    hint button

--- src/engine/actions/newGame.ts
+++ src/engine/actions/newGame.ts
@@ -1,5 +1,12 @@
 import generate from '../generator';
+import { solve } from '../solver';
 
-const newGame = ({ action, ...options }: NewGame, _state?: State) => generate(options);
+const newGame = ({ action, ...options }: NewGame, _state?: State) => {
+  while (true) {
+    const game = generate(options);
+    const solutions = Array.from(solve(game, { max: 1 }));
+    if (solutions.length) return game;
+  }
+};
 
 export default newGame;
