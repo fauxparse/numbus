66faca1b901c46b043482f47a7bec2a500e208e7
Matt Powell <fauxparse@gmail.com>
2022-03-16 13:34:55 +1300

    fix applyStep

--- src/engine/solver/applyStep.ts
+++ src/engine/solver/applyStep.ts
@@ -1,15 +1,26 @@
 import actions from '../actions';
 
 export const applyStep = (step: Step, state: State): State => {
-  const row = state.rows.length - 1;
-  const left = state.cards.find((c) => c?.number === step.left) as Card;
-  const right = state.cards.find((c) => c?.number === step.right && c !== left) as Card;
+  const rowIndex = state.rows.length - 1;
 
-  return actions.use(
-    { row, side: 'right', card: right },
-    actions.operate(
-      { operator: step.operator, row },
-      actions.use({ row, side: 'left', card: left }, state)
-    )
-  );
+  let newState = state;
+  let row = newState.rows[rowIndex];
+
+  if (step.left !== row.left?.number) {
+    const left = newState.cards.find((c) => c?.number === step.left) as Card;
+    newState = actions.use({ row: rowIndex, side: 'left', card: left }, newState);
+    row = newState.rows[rowIndex];
+  }
+
+  if (row.operator !== step.operator) {
+    newState = actions.operate({ row: rowIndex, operator: step.operator }, newState);
+    row = newState.rows[rowIndex];
+  }
+
+  if (step.right !== row.right?.number) {
+    const right = newState.cards.find((c) => c?.number === step.right) as Card;
+    newState = actions.use({ row: rowIndex, side: 'right', card: right }, newState);
+  }
+
+  return newState;
 };
